07a0f0b875e1c7fdb076babd8ec67302bbe1c90f
eduardo <edarij8@gmail.com>
2020-04-26 19:40:37 +0600

    add basket

--- pizzashop/views.py
+++ pizzashop/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from .models import Item
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     items = Item.objects.filter()
     return render(request, 'pizzashop/index.html', locals())
-    
+
 
 def item(request, item_id):
     item = Item.objects.get(id=item_id)
+    session_key = request.session.session_key
+    if not session_key:
+        request.session.cicle_key()
+
     return render(request, 'pizzashop/item.html', locals())
+
+
+def basket_adding(request):
+    return_dict = dict()
+    session_key = request.session.session_key
+    print(request.POST)
+
+    return JsonResponse(return_dict)
